2b9d91ebfdb37752fe2af31763508b31c5ec98ac
LucasQ <mallevo13@gmail.com>
2019-08-25 09:54:26 -0300

    promises

---
+++ async/promise.js
@@ -0,0 +1,33 @@
+var myPromisse = function() {
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/lucasq');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 20) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject('Erro na requisição');
+                }
+            }
+        }
+    });
+}
+// var result = myPromisse()
+//     .then(function(response) {
+//         console.log(response);
+//     })
+//     .catch(function (params) {
+//         console.warn(error);
+//     });
+
+// utilizando o axios
+axios.get('https://api.github.com/users/lucaasq')
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function (params) {
+        console.warn(params);
+    });
