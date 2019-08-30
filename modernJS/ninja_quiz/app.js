1ea630edd259bd8c4123b27ffb046ddf004bbfde
LucasQ <mallevo13@gmail.com>
2019-08-30 08:12:00 -0300

    interactive ninja quiz

---
+++ modernJS/ninja_quiz/app.js
@@ -0,0 +1,33 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('div.result');
+const textResult = document.querySelector('body > div.result > div > p > span');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
+    
+    result.classList.remove('d-none');
+    
+    scrollTo(0,0);  
+
+    let output = 0;
+    const timer = setInterval(() => {
+        textResult.textContent = `${output}%`;
+
+        if (output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+
+});
